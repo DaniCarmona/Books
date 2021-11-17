5d27b9bf9e1129aaf038c848c503070133cc8eff
Daniel Carmona <carmona240101@hotmail.com>
2021-11-17 08:59:14 +0000

    ADD SEVERAL BOOKS TO TEST PAGINATION

--- Startup.cs
+++ Startup.cs
@@ -41,7 +41,7 @@ namespace Books
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, BooksContext booksContext)
         {
             if (env.IsDevelopment())
             {
@@ -69,6 +69,8 @@ namespace Books
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+
+            SeedData.Populate(booksContext);
         }
     }
 }
