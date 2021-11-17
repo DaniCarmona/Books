6b507ffde66e2f334c75f5c30fc6c7b4d756bc29
Daniel Carmona <carmona240101@hotmail.com>
2021-11-17 09:44:31 +0000

    mastra apenas paginas num range de 5

--- ViewModels/PagingInfo.cs
+++ ViewModels/PagingInfo.cs
@@ -7,6 +7,7 @@ namespace Books.ViewModels
 {
     public class PagingInfo
     {
+        public const int NUMBER_OF_PAGES_TO_SHOW_BEFORE_AND_AFTER=5;
         public int TotalItems { get; set; }
         public int PageSize { get; set; } = 10;
         public int CurrentPage { get; set; }
